f202307392595779ab7fc54f7ea28b3025a87737
Phan Ngoc Huan <huanpn1992@gmail.com>
2015-06-25 13:30:59 +0700

    First commit

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/shop20/admin/');
+define('HTTP_CATALOG', 'http://localhost/shop20/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/shop20/admin/');
+define('HTTPS_CATALOG', 'http://localhost/shop20/');
+
+// DIR
+define('DIR_APPLICATION', 'D:\xampp\htdocs\shop20/admin/');
+define('DIR_SYSTEM', 'D:\xampp\htdocs\shop20/system/');
+define('DIR_LANGUAGE', 'D:\xampp\htdocs\shop20/admin/language/');
+define('DIR_TEMPLATE', 'D:\xampp\htdocs\shop20/admin/view/template/');
+define('DIR_CONFIG', 'D:\xampp\htdocs\shop20/system/config/');
+define('DIR_IMAGE', 'D:\xampp\htdocs\shop20/image/');
+define('DIR_CACHE', 'D:\xampp\htdocs\shop20/system/cache/');
+define('DIR_DOWNLOAD', 'D:\xampp\htdocs\shop20/system/download/');
+define('DIR_UPLOAD', 'D:\xampp\htdocs\shop20/system/upload/');
+define('DIR_LOGS', 'D:\xampp\htdocs\shop20/system/logs/');
+define('DIR_MODIFICATION', 'D:\xampp\htdocs\shop20/system/modification/');
+define('DIR_CATALOG', 'D:\xampp\htdocs\shop20/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'shop20');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/shop20/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/shop20/');
+
+// DIR
+define('DIR_APPLICATION', 'D:\xampp\htdocs\shop20/catalog/');
+define('DIR_SYSTEM', 'D:\xampp\htdocs\shop20/system/');
+define('DIR_LANGUAGE', 'D:\xampp\htdocs\shop20/catalog/language/');
+define('DIR_TEMPLATE', 'D:\xampp\htdocs\shop20/catalog/view/theme/');
+define('DIR_CONFIG', 'D:\xampp\htdocs\shop20/system/config/');
+define('DIR_IMAGE', 'D:\xampp\htdocs\shop20/image/');
+define('DIR_CACHE', 'D:\xampp\htdocs\shop20/system/cache/');
+define('DIR_DOWNLOAD', 'D:\xampp\htdocs\shop20/system/download/');
+define('DIR_UPLOAD', 'D:\xampp\htdocs\shop20/system/upload/');
+define('DIR_MODIFICATION', 'D:\xampp\htdocs\shop20/system/modification/');
+define('DIR_LOGS', 'D:\xampp\htdocs\shop20/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'shop20');
+define('DB_PREFIX', 'oc_');
